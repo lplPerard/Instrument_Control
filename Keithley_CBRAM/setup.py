43705e17b1f982b251fe5388ff1e55b0cab5012b
lplPerard <lpl.perard@gmail.com>
2020-02-11 10:56:29 +0100

    added exception for NIVISA

--- Keithley_CBRAM/setup.py
+++ Keithley_CBRAM/setup.py
@@ -1,16 +1,6 @@
-
 # -*- coding: utf-8 -*-
 
-# A simple setup script to create an executable using Tkinter. This also
-# demonstrates the method for creating a Windows executable that does not have
-# an associated console.
-#
-# SimpleTkApp.py is a very simple type of Tkinter application
-#
-# Run the build process by running the command 'python setup.py build'
-#
-# If everything works well you should find a subdirectory in the build
-# subdirectory that contains the files needed to run the application
+# A simple setup script to create an executable using Tkinter.
 
 import sys
 from cx_Freeze import setup, Executable
@@ -24,7 +14,7 @@ executables = [
 ]
 
 setup(name='CBRAM testbench',
-      version='0.1',
+      version='0.2',
       description='Testbench to evaluate CBRAM cells for RF switche applications',
       executables=executables
       )
